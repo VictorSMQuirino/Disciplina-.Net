b2cab96cda2b3a0a3d186d085bee9e9e7814c546
Víctor Souza Moreira Quirino <victorsouzaq@gmail.com>
2023-11-09 17:19:44 -0300

    Primeiro projeto trabalhado em sala de aula

---
+++ Semana1/primeiroProjeto/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+#region Capacidade e conversão de tipos
+int tipoInteiro;
+double tipoDouble;
+string tipoString;
+long tipoLong;
+
+tipoInteiro = int.MaxValue;
+tipoLong = long.MaxValue;
+
+tipoLong = tipoInteiro;
+
+tipoString = "100";
+tipoInteiro = int.Parse(tipoString);
+
+Console.WriteLine("O máximo inteiro é " + tipoInteiro);
+Console.WriteLine("O máximo long é " + tipoLong);
+
+#endregion
+
+#region Operadores
+    tipoDouble = tipoInteiro + tipoLong;
+    // tipoInteiro =  tipoDouble + tipoLong;
+    // tipoLong = tipoDouble + tipoInteiro;
+
+    tipoInteiro = 10 > 5 ? 1 : 0;
+#endregion
