a7c345497ac7b7ae8e0ac3fae6e4a8ec7fde09c4
Víctor Quirino <victorsouzaq@gmail.com>
2023-11-16 17:28:23 -0300

    exemplo de uso de DateTime na aula 04

--- Semana2/exemplo1-aula04/Program.cs
+++ Semana2/exemplo1-aula04/Program.cs
@@ -1,20 +1,28 @@
-﻿#region exemplo ReadLine
-Console.WriteLine("Informe uma string:");
-string? str = Console.ReadLine();
-Console.WriteLine($"A string foi {str}");
-#endregion
+﻿// #region exemplo ReadLine
+// Console.WriteLine("Informe uma string:");
+// string? str = Console.ReadLine();
+// Console.WriteLine($"A string foi {str}");
+// #endregion
 
-#region exemplos de List
-List<List<string>> names = new List<List<string>>();
-List<string> names2 = new List<string>();
-names.Add(names2);
-names[0].Add("Alice");
-names[0].Add("Bob");
-names.Add(new List<string>() {"Charlie", "David"});
+// #region exemplos de List
+// List<List<string>> names = new List<List<string>>();
+// List<string> names2 = new List<string>();
+// names.Add(names2);
+// names[0].Add("Alice");
+// names[0].Add("Bob");
+// names.Add(new List<string>() {"Charlie", "David"});
 
-foreach(List<string> list in names){
-    foreach(string name in list){
-        Console.WriteLine(name);
-    }
-}
+// foreach(List<string> list in names){
+//     foreach(string name in list){
+//         Console.WriteLine(name);
+//     }
+// }
+// #endregion
+
+#region exeplos de DateTime
+DateTime now = DateTime.Now;
+Console.WriteLine($"Now: {now}");
+
+DateTime victorBorn = new DateTime(1998, 07, 13);
+Console.WriteLine($"Victor born: {victorBorn.DayOfWeek}");
 #endregion
