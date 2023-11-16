9e0e1088af09edf36c16c9ddb529b2ea0941f740
Víctor Quirino <victorsouzaq@gmail.com>
2023-11-16 17:13:07 -0300

    exemplos de uso de List na aula 04

--- Semana2/exemplo1-aula04/Program.cs
+++ Semana2/exemplo1-aula04/Program.cs
@@ -3,3 +3,18 @@ Console.WriteLine("Informe uma string:");
 string? str = Console.ReadLine();
 Console.WriteLine($"A string foi {str}");
 #endregion
+
+#region exemplos de List
+List<List<string>> names = new List<List<string>>();
+List<string> names2 = new List<string>();
+names.Add(names2);
+names[0].Add("Alice");
+names[0].Add("Bob");
+names.Add(new List<string>() {"Charlie", "David"});
+
+foreach(List<string> list in names){
+    foreach(string name in list){
+        Console.WriteLine(name);
+    }
+}
+#endregion
