11ec4339fe69516bd9ed0e6bd951a1d7989c3010
Víctor Quirino <victorsouzaq@gmail.com>
2023-11-30 15:23:41 -0300

    Início do exercício 5 da aula 8

---
+++ Semana4/exercicio-aula08/Agenda.cs
@@ -0,0 +1,5 @@
+namespace Atividade;
+public class Agenda
+{
+    
+}
---
+++ Semana4/exercicio-aula08/Contato.cs
@@ -0,0 +1,11 @@
+namespace Atividade;
+public class Class
+{
+    private string? nome;
+    private string? numero;
+    public string? Nome => nome;
+    public string? Numero => numero;
+
+    
+
+}
